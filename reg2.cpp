1d83e99803c6605b2dbabd29ad908454f47d8694
J. Junquera <javier.junquera.sanchez@protonmail.com>
2019-08-09 17:48:26 +0200

    Cálculos completos

--- reg2.cpp
+++ reg2.cpp
@@ -94,6 +94,8 @@ void regresion_cuadratica(LweSample* a, LweSample* b, LweSample* c, const vector
     bootsCOPY(&uno[ci], &aux2[ci], bk);
   }
 
+  cout << "Variables inicializadas" << endl;
+
   for(int ci=0 ; ci < values; ci++){
     LweSample* x = xs[ci];
     LweSample* y = ys[ci];
@@ -112,6 +114,7 @@ void regresion_cuadratica(LweSample* a, LweSample* b, LweSample* c, const vector
     multiply(xy, x, y, nb_bits, bk);
     multiply(x2y, x2, y, nb_bits, bk);
 
+    cout << "xys" << ci << endl;
   }
 
   LweSample* i2 = new_gate_bootstrapping_ciphertext_array(nb_bits, bk->params);
@@ -123,6 +126,8 @@ void regresion_cuadratica(LweSample* a, LweSample* b, LweSample* c, const vector
   h_pow(j2, j, 2, nb_bits, bk);
   h_pow(k2, k, 2, nb_bits, bk);
   h_pow(l2, k, 2, nb_bits, bk);
+
+  cout << "Cuadrados i,j,k,l" << endl;
 
   LweSample* il = new_gate_bootstrapping_ciphertext_array(nb_bits, bk->params);
   LweSample* jk = new_gate_bootstrapping_ciphertext_array(nb_bits, bk->params);
@@ -143,6 +148,8 @@ void regresion_cuadratica(LweSample* a, LweSample* b, LweSample* c, const vector
   multiply(ul, u, l, nb_bits, bk);
   multiply(vl, v, l, nb_bits, bk);
   multiply(wj, w, j, nb_bits, bk);
+
+  cout << "Duplas" << endl;
 
   LweSample* ijkl = new_gate_bootstrapping_ciphertext_array(nb_bits, bk->params);
   LweSample* i2l2 = new_gate_bootstrapping_ciphertext_array(nb_bits, bk->params);
@@ -166,11 +173,13 @@ void regresion_cuadratica(LweSample* a, LweSample* b, LweSample* c, const vector
   multiply_float(jl2n, jl, ln, float_bits, nb_bits, bk);
   multiply_float(k2ln, k2, ln, float_bits, nb_bits, bk);
 
+  cout << "Complejos" << endl;
+
   /*
-  c = (ul - wj)/(ln - j2)
-  c += (jklv - jk2w - il2v + iklw)/(jl2n - k2ln - j3l + j2k2)
-  r = (j2k2-2(ijkl)+i2l2)/(jl2n - (k2ln - j3l + j2k2))
-  c = c / (1 - r)
+    c = (ul - wj)/(ln - j2)
+    c += (jklv - jk2w - il2v + iklw)/(jl2n - k2ln - j3l + j2k2)
+    r = (j2k2-2(ijkl)+i2l2)/(jl2n - (k2ln - j3l + j2k2))
+    c = c / (1 - r)
   */
 
   // c = (ul - wj)/(ln - j2)
@@ -192,6 +201,8 @@ void regresion_cuadratica(LweSample* a, LweSample* b, LweSample* c, const vector
   for(int ci=0; ci < nb_bits; ci++)
     bootsCOPY(&c[ci], &aux1[ci], bk);
 
+  cout << "Pre r" << endl;
+
   // r = (j2k2-2(ijkl)+i2l2)/(jl2n - k2ln - j3l + j2k2)
   LweSample* r = new_gate_bootstrapping_ciphertext_array(nb_bits, bk->params);
   sum(aux1, ijkl, ijkl, nb_bits, bk);
@@ -208,5 +219,34 @@ void regresion_cuadratica(LweSample* a, LweSample* b, LweSample* c, const vector
   divide_float(aux2, c, aux1, float_bits, nb_bits, bk);
   for(int ci=0; ci < nb_bits; ci++)
     bootsCOPY(&c[ci], &aux2[ci], bk);
+
+  cout << "c" << endl;
+
+  /*
+    b = (vl - kw + cjk - cil)/(jl - k2)
+  */
+  LweSample* cjk = new_gate_bootstrapping_ciphertext_array(nb_bits, bk->params);
+  LweSample* cil = new_gate_bootstrapping_ciphertext_array(nb_bits, bk->params);
+
+  multiply_float(cjk, c, jk, float_bits, nb_bits, bk);
+  multiply_float(cil, c, jk, float_bits, nb_bits, bk);
+  resta(aux1, cjk, cil, nb_bits, bk);
+  resta(aux2, vl, kw, nb_bits, bk);
+  sum(aux3, aux1, aux2, nb_bits, bk);
+  resta(aux1, jl, k2, nb_bits, bk);
+  divide_float(b, aux3, aux1, float_bits, nb_bits, bk);
+
+  cout << "b" << endl;
+
+  /*
+    a = (w - (bk + cj)) / l
+  */
+  multiply_float(aux1, b, k, float_bits, nb_bits, bk);
+  multiply_float(aux2, c, j, float_bits, nb_bits, bk);
+  suma(aux3, aux1, aux2, nb_bits, bk);
+  resta(aux1, w, aux3, nb_bits, bk);
+  divide_float(a, aux1, l, float_bits, nb_bits, bk);
+
+  cout << "a" << endl;
 
 }
