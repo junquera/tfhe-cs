3a5e15c4ab809c7aef3f0a57cced8111d9cf1de9
J. Junquera <javier.junquera.sanchez@protonmail.com>
2019-08-10 14:14:19 +0200

    Client main

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ export LIBRARY_PATH=$LIBRARY_PATH:$TFHE_PREFIX/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TFHE_PREFIX/lib
 
 g++ main.cpp functions.cpp reg2.cpp functions.h reg2.h client.cpp client.h server.cpp server.h aux.cpp aux.h  -o main -ltfhe-fftw
+g++ client_main.cpp functions.cpp reg2.cpp functions.h reg2.h client.cpp client.h server.cpp server.h aux.cpp aux.h  -o client -ltfhe-fftw
