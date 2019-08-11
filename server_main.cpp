978cd4cc97084e3d24d833bad4af8ad083753931
J. Junquera <javier@junquera.xyz>
2019-08-11 20:20:32 +0200

    Menu for server_main

--- server_main.cpp
+++ server_main.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 int main(){
 
@@ -19,19 +21,32 @@ int main(){
 
   int values = 48;
 
-  vector<LweSample*> xs, ys;
 
+  string data_ub, data_name, res_dest;
+
+  cout << "¿Dónde están los datos? > ";
+  cin >> data_ub;
+  cout << "¿Cómo se llaman (ciudad_A, ciudad_B...)? > ";
+  cin >> data_name;
+  cout << "¿Dónde quieres guardar los resultados? > ";
+  cin >> res_dest;
+  // if(mkdir(res_dest.c_str(), 0644) == 0){
+  //   throw "Error creating folder!";
+  // }
+
+  vector<LweSample*> xs, ys;
   stringstream stringStream;
   for(int i = 0; i < values; i++){
     LweSample* resultX = new_gate_bootstrapping_ciphertext_array(nb_bits, params);
     stringStream.str("");
-    stringStream << "ciudad_AX" << i;
+    // TODO Cambiar data_ub << "/" por path.join(data_ub, data_name)
+    stringStream << data_ub << "/" << data_name << "X" << i;
     retrieveResult(stringStream.str(), resultX, nb_bits, params);
     xs.push_back(resultX);
 
     LweSample* resultY = new_gate_bootstrapping_ciphertext_array(nb_bits, params);
     stringStream.str("");
-    stringStream << "ciudad_AY" << i;
+    stringStream << data_ub << "/" << data_name << "Y" << i;
     retrieveResult(stringStream.str(), resultY, nb_bits, params);
     ys.push_back(resultY);
   }
@@ -41,7 +56,7 @@ int main(){
   LweSample* c = new_gate_bootstrapping_ciphertext_array(nb_bits, params);
 
   HServer server(nb_bits, float_bits);
-  server.regresionCuadratica(a, b, c, xs, ys, "cloud.key", "results");
+  server.regresionCuadratica(a, b, c, xs, ys, "cloud.key", res_dest);
 
   return 0;
 }
