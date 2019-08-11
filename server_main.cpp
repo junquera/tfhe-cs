58f61cba2f3a544c3883eb0a6c18f765f4f819e0
J. Junquera <javier@junquera.xyz>
2019-08-11 11:26:06 +0200

    Minor changes

--- server_main.cpp
+++ server_main.cpp
@@ -13,12 +13,15 @@ int main(){
 	//if necessary, the params are inside the key
 	params = (TFheGateBootstrappingParameterSet*) bk->params;
 
+
   int nb_bits = 32;
   int float_bits = 7;
 
   vector<LweSample*> xs, ys;
 
+
   LweSample* result = new_gate_bootstrapping_ciphertext_array(nb_bits, params);
+
   stringstream stringStream;
   for(int i = 0; i < 12; i++){
 
@@ -26,7 +29,6 @@ int main(){
     stringStream << "ciudad_AX" << i;
     retrieveResult(stringStream.str(), result, nb_bits, params);
     xs.push_back(result);
-
 
     stringStream.str("");
     stringStream << "ciudad_AY" << i;
@@ -39,7 +41,7 @@ int main(){
   LweSample* c = new_gate_bootstrapping_ciphertext_array(nb_bits, params);
 
   HServer server(nb_bits, float_bits);
-  server.regresionCuadratica(a, b, c,xs, ys, "cloud.key", "results");
+  server.regresionCuadratica(a, b, c, xs, ys, "cloud.key", "results");
 
   return 0;
 }
