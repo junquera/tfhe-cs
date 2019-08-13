a26321e0c7c0c3b3a45d5ae3d4ffb6b514d89e64
J. Junquera <javier@junquera.xyz>
2019-08-13 08:31:32 +0200

    Makefile + Refactor

--- Makefile
+++ Makefile
@@ -1,5 +1,21 @@
-all:
-	bash build.sh
+TFHE_PREFIX=/usr/local #the prefix where you installed tfhe
+C_INCLUDE_PATH=$C_INCLUDE_PATH:$TFHE_PREFIX/include
+CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:$TFHE_PREFIX/include
+LIBRARY_PATH=$LIBRARY_PATH:$TFHE_PREFIX/lib
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TFHE_PREFIX/lib
 
-launch: all
-	./main
+
+all: test server client
+	echo "Ok!"
+
+test:
+	g++ main.cpp functions.cpp reg2.cpp functions.h reg2.h client.cpp client.h server.cpp server.h aux.cpp aux.h  -o test -ltfhe-fftw
+
+server:
+	g++ server_main.cpp functions.cpp reg2.cpp functions.h reg2.h client.cpp client.h server.cpp server.h aux.cpp aux.h  -o server -ltfhe-fftw
+
+client:
+	g++ client_main.cpp functions.cpp reg2.cpp functions.h reg2.h client.cpp client.h server.cpp server.h aux.cpp aux.h  -o client -ltfhe-fftw
+
+clean:
+	rm main server client
--- server.h
+++ server.h
@@ -2,70 +2,7 @@
 #define _SERVER_H_INCLUDED_
 
 #include "aux.h"
-class RegresionCuadratica {
-  public:
-    RegresionCuadratica(int _nb_bits, int _float_bits, TFheGateBootstrappingCloudKeySet* _bk);
-    void calcula(LweSample* a, LweSample* b, LweSample* c, const vector<LweSample*> xs, const vector<LweSample*> ys, string results_path);
-  private:
-    int nb_bits;
-    int float_bits;
-
-    TFheGateBootstrappingCloudKeySet* bk;
-
-    void initVectores(const vector<LweSample*> xs, const vector<LweSample*> ys, string results_path);
-    void calcCuadrados(string results_path);
-    void calcDuplas(string results_path);
-    void calcComplejos(string results_path);
-    void calcC(LweSample* c, string results_path);
-    void calcB(LweSample* b, LweSample* c, string results_path);
-    void calcA(LweSample* a, LweSample* b, LweSample* c, string results_path);
-
-
-    LweSample* aux1;
-    LweSample* aux2;
-    LweSample* aux3;
-    LweSample* aux4;
-
-    LweSample* n;
-    LweSample* uno;
-
-    LweSample* i;
-    LweSample* j;
-    LweSample* k;
-    LweSample* l;
-
-    LweSample* u;
-    LweSample* v;
-    LweSample* w;
-
-    // Cuadrados de las variables
-    LweSample* i2;
-    LweSample* j2;
-    LweSample* k2;
-    LweSample* l2;
-
-    // Duplas
-    LweSample* il;
-    LweSample* jk;
-    LweSample* jl;
-    LweSample* kw;
-    LweSample* kv;
-    LweSample* ln;
-    LweSample* ul;
-    LweSample* vl;
-    LweSample* wj;
-
-    LweSample* ijkl;
-    LweSample* i2l2;
-    LweSample* iklw;
-    LweSample* il2v;
-    LweSample* j2k2;
-    LweSample* jk2w;
-    LweSample* jklv;
-    LweSample* j3l;
-    LweSample* jl2n;
-    LweSample* k2ln;
-};
+#include "reg2.h"
 
 class HServer {
   public:
